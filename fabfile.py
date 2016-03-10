06e44a9d88791412ed4e6e336e706a2220e04966
jaime <garciadiazjaime@gmail.com>
2016-03-09 19:48:24 -0600

    deploy

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,10 @@
 from fabric.api import local
 
 def update():
-    local('git checkout develop')
+    local('git checkout production')
     local('git pull origin develop')
 
 def deploy():
     local('git add .')
     local('git commit -m "deploy"')
-    local('git push openshift HEAD:master')
+    local('git push upstream HEAD:master')
