08da307c1458ea0ec8c349260bd521da0b92d1ef
jaime <garciadiazjaime@gmail.com>
2018-08-28 19:20:42 -0500

    release: qa

--- wsgi/project/settings/local.py
+++ wsgi/project/settings/local.py
@@ -6,7 +6,7 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'resources/db/contakto.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': 'wsgi/project/resources/db/contakto.sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
--- wsgi/project/settings/prod.py
+++ wsgi/project/settings/prod.py
@@ -5,11 +5,10 @@ import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'system',
-        'USER': os.environ['OPENSHIFT_POSTGRESQL_DB_USERNAME'],                      # Not used with sqlite3.
-        'PASSWORD': os.environ['OPENSHIFT_POSTGRESQL_DB_PASSWORD'],                  # Not used with sqlite3.
-        'HOST': os.environ['OPENSHIFT_POSTGRESQL_DB_HOST'],                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': os.environ['OPENSHIFT_POSTGRESQL_DB_PORT'],                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.environ['CONTAKTO_DB_NAME'],
+        'USER': os.environ['CONTAKTO_DB_USER'],                      # Not used with sqlite3.
+        'PASSWORD': os.environ['CONTAKTO_DB_PASSWORD'],                  # Not used with sqlite3.
+        'HOST': os.environ['MYSQL_CONTAKTO_PORT_3306_TCP_ADDR']                      # Set to empty string for localhost. Not used with sqlite3.
     }
 }
