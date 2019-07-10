b92e9d48e1aa98ceaaef2270cebf1902aa4c5efd
jaime <jgarciadiaz@groupon.com>
2019-07-09 20:23:03 -0500

    fix: add checks for [0]

--- project/app/investigacion/views.py
+++ project/app/investigacion/views.py
@@ -23,9 +23,9 @@ def print_reporte_laboral(request, investigacion_id):
 	tel_recado = telefonos.filter(categoria='recado')[0] if telefonos.filter(categoria='recado').count() else ''
 
 	trayectoria = candidato.trayectorialaboral_set.filter(status=True, visible_en_status=True)
-	domicilio = candidato.direccion_set.all()[0]
+	domicilio = candidato.direccion_set.all()[0] if candidato.direccion_set.all().count() else None
 	
-	origen = candidato.origen_set.all()[0]
+	origen = candidato.origen_set.all()[0] if candidato.origen_set.all().count() else None
 	fecha_nacimiento = origen.fecha
 
 	adjuntos = investigacion.adjuntos_set.all()[0] if investigacion.adjuntos_set.all().count() else None
@@ -48,9 +48,9 @@ def print_reporte_socioeconomico(request, investigacion_id):
 	tel_recado = telefonos.filter(categoria='recado')[0] if telefonos.filter(categoria='recado').count() else ''
 
 	trayectoria = candidato.trayectorialaboral_set.filter(status=True, visible_en_status=True)
-	domicilio = investigacion.entrevistadireccion_set.all()[0]
+	domicilio = investigacion.entrevistadireccion_set.all()[0] if investigacion.entrevistadireccion_set.all().count() else None
 	
-	origen = candidato.origen_set.all()[0]
+	origen = candidato.origen_set.all()[0] if candidato.origen_set.all().count() else None
 	fecha_nacimiento = origen.fecha
 
 	adjuntos = investigacion.adjuntos_set.all()[0] if investigacion.adjuntos_set.all().count() else None
@@ -58,22 +58,22 @@ def print_reporte_socioeconomico(request, investigacion_id):
 
 	demanda = candidato.demanda_set.all()[0] if candidato.demanda_set.all().count() else None
 
-	entrevista_persona = investigacion.entrevistapersona_set.all()[0]
+	entrevista_persona = investigacion.entrevistapersona_set.all()[0] if investigacion.entrevistapersona_set.all().count() else None
 
-	entrevista_investigacion = investigacion.entrevistainvestigacion_set.all()[0]
-	entrevista_cita = investigacion.entrevistacita_set.all()[0]
+	entrevista_investigacion = investigacion.entrevistainvestigacion_set.all()[0] if investigacion.entrevistainvestigacion_set.all().count() else None
+	entrevista_cita = investigacion.entrevistacita_set.all()[0] if investigacion.entrevistacita_set.all().count() else None
 
-	licencia = entrevista_persona.entrevistalicencia_set.all()[0]
-	historial_en_empresa = entrevista_persona.entrevistahistorialenempresa_set.filter(categoria='trabajo')[0]
-	historial_familiar_en_empresa = entrevista_persona.entrevistahistorialenempresa_set.filter(categoria='familiar')[0]
-	info_personal = entrevista_persona.entrevistainfopersonal_set.all()[0]
-	salud = entrevista_persona.entrevistasalud_set.all()[0]
-	actividades_habitos = entrevista_persona.entrevistaactividadeshabitos_set.all()[0]
+	licencia = entrevista_persona.entrevistalicencia_set.all()[0] if entrevista_persona.entrevistalicencia_set.all().count() else None
+	historial_en_empresa = entrevista_persona.entrevistahistorialenempresa_set.filter(categoria='trabajo')[0] if entrevista_persona.entrevistahistorialenempresa_set.filter(categoria='trabajo').count() else None
+	historial_familiar_en_empresa = entrevista_persona.entrevistahistorialenempresa_set.filter(categoria='familiar')[0] if entrevista_persona.entrevistahistorialenempresa_set.filter(categoria='familiar').count() else None
+	info_personal = entrevista_persona.entrevistainfopersonal_set.all()[0] if entrevista_persona.entrevistainfopersonal_set.all().count() else None
+	salud = entrevista_persona.entrevistasalud_set.all()[0] if entrevista_persona.entrevistasalud_set.all().count() else None
+	actividades_habitos = entrevista_persona.entrevistaactividadeshabitos_set.all()[0] if entrevista_persona.entrevistaactividadeshabitos_set.all().count() else None
 
 	#ADADEMICA
-	info_academica = entrevista_persona.entrevistaacademica_set.all()[0]
-	info_academica_grados = entrevista_persona.entrevistagradoescolaridad_set.all()
-	info_academica_idioma = entrevista_persona.entrevistaotroidioma_set.all()[0]
+	info_academica = entrevista_persona.entrevistaacademica_set.all()[0] if entrevista_persona.entrevistaacademica_set.all().count() else None
+	info_academica_grados = entrevista_persona.entrevistagradoescolaridad_set.all() if entrevista_persona.entrevistagradoescolaridad_set.all().count() else None
+	info_academica_idioma = entrevista_persona.entrevistaotroidioma_set.all()[0] if entrevista_persona.entrevistaotroidioma_set.all().count() else None
 
 
 	return render_to_response('sections/reportes/socioeconomico/index.html', locals(), context_instance=RequestContext(request))
