67f7d94e17af03e2dab57b632dcbc649b88095a5
jaime <jgarciadiaz@groupon.com>
2019-08-19 20:41:16 -0500

    fix: add checks for entrevista_persona

--- project/app/investigacion/views.py
+++ project/app/investigacion/views.py
@@ -56,47 +56,46 @@ def print_reporte_socioeconomico(request, investigacion_id):
 	demanda = candidato.demanda_set.all()[0] if candidato.demanda_set.all().count() else None
 
 	entrevista_persona = investigacion.entrevistapersona_set.all()[0] if investigacion.entrevistapersona_set.all().count() else None
-
-	origen = entrevista_persona.entrevistaorigen_set.get() if entrevista_persona.entrevistaorigen_set.all().count() else None
+	origen = entrevista_persona.entrevistaorigen_set.get() if entrevista_persona and entrevista_persona.entrevistaorigen_set.all().count() else None
 
 	entrevista_investigacion = investigacion.entrevistainvestigacion_set.all()[0] if investigacion.entrevistainvestigacion_set.all().count() else None
 	entrevista_cita = investigacion.entrevistacita_set.all()[0] if investigacion.entrevistacita_set.all().count() else None
 
-	licencia = entrevista_persona.entrevistalicencia_set.all()[0] if entrevista_persona.entrevistalicencia_set.all().count() else None
-	historial_en_empresa = entrevista_persona.entrevistahistorialenempresa_set.filter(categoria='trabajo')[0] if entrevista_persona.entrevistahistorialenempresa_set.filter(categoria='trabajo').count() else None
-	historial_familiar_en_empresa = entrevista_persona.entrevistahistorialenempresa_set.filter(categoria='familiar')[0] if entrevista_persona.entrevistahistorialenempresa_set.filter(categoria='familiar').count() else None
-	info_personal = entrevista_persona.entrevistainfopersonal_set.all()[0] if entrevista_persona.entrevistainfopersonal_set.all().count() else None
-	salud = entrevista_persona.entrevistasalud_set.all()[0] if entrevista_persona.entrevistasalud_set.all().count() else None
-	actividades_habitos = entrevista_persona.entrevistaactividadeshabitos_set.all()[0] if entrevista_persona.entrevistaactividadeshabitos_set.all().count() else None
+	licencia = entrevista_persona.entrevistalicencia_set.all()[0] if entrevista_persona and entrevista_persona.entrevistalicencia_set.all().count() else None
+	historial_en_empresa = entrevista_persona.entrevistahistorialenempresa_set.filter(categoria='trabajo')[0] if entrevista_persona and entrevista_persona.entrevistahistorialenempresa_set.filter(categoria='trabajo').count() else None
+	historial_familiar_en_empresa = entrevista_persona.entrevistahistorialenempresa_set.filter(categoria='familiar')[0] if entrevista_persona and entrevista_persona.entrevistahistorialenempresa_set.filter(categoria='familiar').count() else None
+	info_personal = entrevista_persona.entrevistainfopersonal_set.all()[0] if entrevista_persona and entrevista_persona.entrevistainfopersonal_set.all().count() else None
+	salud = entrevista_persona.entrevistasalud_set.all()[0] if entrevista_persona and entrevista_persona.entrevistasalud_set.all().count() else None
+	actividades_habitos = entrevista_persona.entrevistaactividadeshabitos_set.all()[0] if entrevista_persona and entrevista_persona.entrevistaactividadeshabitos_set.all().count() else None
 
 	#ADADEMICA
-	info_academica = entrevista_persona.entrevistaacademica_set.all()[0] if entrevista_persona.entrevistaacademica_set.all().count() else None
-	info_academica_grados = entrevista_persona.entrevistagradoescolaridad_set.all() if entrevista_persona.entrevistagradoescolaridad_set.all().count() else None
-	info_academica_idioma = entrevista_persona.entrevistaotroidioma_set.all()[0] if entrevista_persona.entrevistaotroidioma_set.all().count() else None
+	info_academica = entrevista_persona.entrevistaacademica_set.all()[0] if entrevista_persona and entrevista_persona.entrevistaacademica_set.all().count() else None
+	info_academica_grados = entrevista_persona.entrevistagradoescolaridad_set.all() if entrevista_persona and entrevista_persona.entrevistagradoescolaridad_set.all().count() else None
+	info_academica_idioma = entrevista_persona.entrevistaotroidioma_set.all()[0] if entrevista_persona and entrevista_persona.entrevistaotroidioma_set.all().count() else None
 
-	referencias = entrevista_persona.entrevistareferencia_set.all() if entrevista_persona.entrevistareferencia_set.all().count() else None
+	referencias = entrevista_persona.entrevistareferencia_set.all() if entrevista_persona and entrevista_persona.entrevistareferencia_set.all().count() else None
 
-	marco_familiar = entrevista_persona.entrevistamiembromarcofamiliar_set.all() if entrevista_persona.entrevistamiembromarcofamiliar_set.all().count() else None
+	marco_familiar = entrevista_persona.entrevistamiembromarcofamiliar_set.all() if entrevista_persona and entrevista_persona.entrevistamiembromarcofamiliar_set.all().count() else None
 
 	# vivienda
-	situacion_vivienda = entrevista_persona.entrevistasituacionvivienda_set.get()
-	propietario_vivienda = entrevista_persona.entrevistapropietariovivienda_set.get()
-	caracteristicas_vivienda = entrevista_persona.entrevistacaractaristicasvivienda_set.get()
-	tipo_inmueble_vivienda = entrevista_persona.entrevistatipoinmueble_set.get()
-	distribucion_vivienda = entrevista_persona.entrevistadistribuciondimensiones_set.get()
+	situacion_vivienda = entrevista_persona.entrevistasituacionvivienda_set.get() if entrevista_persona else None
+	propietario_vivienda = entrevista_persona.entrevistapropietariovivienda_set.get() if entrevista_persona else None
+	caracteristicas_vivienda = entrevista_persona.entrevistacaractaristicasvivienda_set.get() if entrevista_persona else None
+	tipo_inmueble_vivienda = entrevista_persona.entrevistatipoinmueble_set.get() if entrevista_persona else None
+	distribucion_vivienda = entrevista_persona.entrevistadistribuciondimensiones_set.get() if entrevista_persona else None
 
 	# economica
-	ingresos = entrevista_persona.entrevistaeconomica_set.filter(tipo='ingreso')
-	egresos = entrevista_persona.entrevistaeconomica_set.filter(tipo='egreso')
+	ingresos = entrevista_persona.entrevistaeconomica_set.filter(tipo='ingreso') if entrevista_persona else None
+	egresos = entrevista_persona.entrevistaeconomica_set.filter(tipo='egreso') if entrevista_persona else None
 
-	tarjeta_credito = entrevista_persona.entrevistatarjetacreditocomercial_set.all()
-	tarjeta_debito = entrevista_persona.entrevistacuentadebito_set.all()
-	automoviles = entrevista_persona.entrevistaautomovil_set.all()
-	bienes_raices = entrevista_persona.entrevistabienesraices_set.all()
-	seguros = entrevista_persona.entrevistaseguro_set.all()
-	deudas = entrevista_persona.entrevistadeudaactual_set.all()
+	tarjeta_credito = entrevista_persona.entrevistatarjetacreditocomercial_set.all() if entrevista_persona else None
+	tarjeta_debito = entrevista_persona.entrevistacuentadebito_set.all() if entrevista_persona else None
+	automoviles = entrevista_persona.entrevistaautomovil_set.all() if entrevista_persona else None
+	bienes_raices = entrevista_persona.entrevistabienesraices_set.all() if entrevista_persona else None
+	seguros = entrevista_persona.entrevistaseguro_set.all() if entrevista_persona else None
+	deudas = entrevista_persona.entrevistadeudaactual_set.all() if entrevista_persona else None
 
-	infonavit = entrevista_persona.entrevistaprestacionvivienda_set.get(categoria_viv='infonavit') if entrevista_persona.entrevistaprestacionvivienda_set.filter(categoria_viv='infonavit').count() else None
-	fonacot = entrevista_persona.entrevistaprestacionvivienda_set.get(categoria_viv='fonacot') if entrevista_persona.entrevistaprestacionvivienda_set.filter(categoria_viv='fonacot').count() else None
+	infonavit = entrevista_persona.entrevistaprestacionvivienda_set.get(categoria_viv='infonavit') if entrevista_persona and entrevista_persona.entrevistaprestacionvivienda_set.filter(categoria_viv='infonavit').count() else None
+	fonacot = entrevista_persona.entrevistaprestacionvivienda_set.get(categoria_viv='fonacot') if entrevista_persona and entrevista_persona.entrevistaprestacionvivienda_set.filter(categoria_viv='fonacot').count() else None
 
 	return render_to_response('sections/reportes/socioeconomico/index.html', locals(), context_instance=RequestContext(request))
