a82143fc55d5561e361254b45b4f2a7bbc590642
jaime <garciadiazjaime@gmail.com>
2020-01-15 11:39:29 -0600

    feat: [99-urgente-falla-15-01-2020] accept null

--- project/app/persona/models.py
+++ project/app/persona/models.py
@@ -609,7 +609,7 @@ class Opinion(models.Model):
 	puesto = models.CharField(max_length=140, null=True, blank=True)
 	telefono = models.CharField(max_length=140, null=True, blank=True)
 	email = models.CharField(max_length=140, null=True, blank=True)
-	referencia = models.IntegerField(default=0, choices=ACTIVO_OPCIONES)
+	referencia = models.IntegerField(default=0, choices=ACTIVO_OPCIONES, null=True, blank=True)
 
 	def __unicode__(self):
 		return  '%s' % (self.trayectoriaLaboral)
