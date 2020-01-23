0ae9273c9690e2e2be688569e1d0f59e54b8fe41
jaime <garciadiazjaime@gmail.com>
2020-01-22 20:11:12 -0600

    feat: inclue sha1

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 from fabric.api import local
 
 def deploy():
+  local('git rev-parse HEAD > SHA1.txt')
   local('docker build -t garciadiazjaime/admin-contakto .')
   local('docker push garciadiazjaime/admin-contakto')
-  local('git rev-parse HEAD > SHA1.txt')
   local('echo "docker pull garciadiazjaime/admin-contakto"')
