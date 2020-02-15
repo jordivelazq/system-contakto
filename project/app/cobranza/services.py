7eee260945149f626a298f0cb05c04f157db6ee5
jaime <garciadiazjaime@gmail.com>
2020-02-14 18:07:39 -0600

    feat: check filtros

--- project/app/cobranza/services.py
+++ project/app/cobranza/services.py
@@ -5,17 +5,18 @@ from app.cobranza.models import Cobranza
 def get_cobranza(filtros_json):
     cobranza = Cobranza.objects.filter(investigacion__status_active=True)
 
-    if len(filtros_json['status_id']) and int(filtros_json['status_id']) > -1:
-      cobranza = cobranza.filter(investigacion__status_general=filtros_json['status_id'])
-    if len(filtros_json['compania_id']):
-      cobranza = cobranza.filter(investigacion__compania__id=filtros_json['compania_id'])
-    if len(filtros_json['contacto_id']):
-      cobranza = cobranza.filter(investigacion__contacto__id=filtros_json['contacto_id'])
-    if len(filtros_json['factura_folio']):
-      if filtros_json['factura_folio'] == 'por-facturar':
-        cobranza = cobranza.filter(folio='')
-      else:
-        cobranza = cobranza.filter(folio=filtros_json['factura_folio'])
+    if filtros_json != None:
+      if 'status_id' in filtros_json and len(filtros_json['status_id']) and int(filtros_json['status_id']) > -1:
+        cobranza = cobranza.filter(investigacion__status_general=filtros_json['status_id'])
+      if 'compania_id' in filtros_json and len(filtros_json['compania_id']):
+        cobranza = cobranza.filter(investigacion__compania__id=filtros_json['compania_id'])
+      if 'contacto_id' in filtros_json and len(filtros_json['contacto_id']):
+        cobranza = cobranza.filter(investigacion__contacto__id=filtros_json['contacto_id'])
+      if 'factura_folio' in filtros_json and len(filtros_json['factura_folio']):
+        if filtros_json['factura_folio'] == 'por-facturar':
+          cobranza = cobranza.filter(folio='')
+        else:
+          cobranza = cobranza.filter(folio=filtros_json['factura_folio'])
     
     cobranza = cobranza.order_by('id')[:500]
 
