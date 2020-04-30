7ef79bb1ff6c40df782f49f7dde390881bae6972
jaime <garciadiazjaime@gmail.com>
2020-04-30 11:39:09 -0500

    feat: exito url

--- project/app/persona/urls.py
+++ project/app/persona/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
 	url(r'^/$', panel, name='panel_persona'),
 	url(r'^nuevo$', crear, name='crear_persona'),
 	url(r'^nuevo/exito$', crear, name='crear_persona'),
+	url(r'^exito$', panel, name='panel_persona'),
 	url(r'^/exito$', panel, name='panel_persona'),
 	url(r'^(?P<investigacion_id>[^/]+)/eliminar$', eliminar, name='eliminar_persona'),
 
