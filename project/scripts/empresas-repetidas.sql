2bab0759651eb8664873d9c2a8e1087ab2d4ef37
jaime <garciadiazjaime@gmail.com>
2021-01-06 21:35:33 -0600

    feat: [https://trello.com/c/zFz5v6EW] update list

--- project/scripts/empresas-repetidas.sql
+++ project/scripts/empresas-repetidas.sql
@@ -1,12 +1,15 @@
 SELECT
   REPLACE(cc.nombre, '.', '') as nombre,
   cc.id,
-  (select count(*) from investigacion_investigacion where compania_id=cc.id) as investigaciones 
+  (select count(*) from investigacion_investigacion where compania_id=cc.id) as investigaciones,
+  (select count(*) from persona_trayectorialaboral trayectoria where trayectoria.compania_id=cc.id) as trayectoria,
+  (select count(*) from compania_contacto contacto where contacto.compania_id=cc.id) as contacto,
+  (select count(*) from compania_sucursales sucursales where sucursales.compania_id=cc.id) as sucursales
 FROM compania_compania cc 
 WHERE id IN (
   [RESULT FROM SELECT - BELOW -]
 )
-ORDER BY investigaciones, nombre;
+ORDER BY nombre;
 
 
 
@@ -17,8 +20,9 @@ FROM
       SELECT
         cc2.nombre,
         count(*) as total,
-        GROUP_CONCAT(cc2.id) as IDS  
+        GROUP_CONCAT(cc2.id) as IDS
       FROM compania_compania cc2
+      WHERE es_cliente = 0
       GROUP BY cc2.nombre
       HAVING total > 1
       ORDER BY cc2.nombre
