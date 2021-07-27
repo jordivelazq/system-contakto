416d9f9276b9ece463de0f2e81998c34847474e9
jaime <garciadiazjaime@gmail.com>
2021-07-26 21:13:53 -0500

    feat: add investigcion api

--- project/app/api/views.py
+++ project/app/api/views.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import HttpResponse
-from app.compania.models import Compania, Contacto
+from app.compania.models import Contacto
 from django.core import serializers
-import json
-from app.reportes.services import ServiceReporte
-from app.util.email import EmailHandler
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 ''' ----------------- EMPRESA -----------------'''
 
 def empresa_get_contactos(request, empresa_id):
 	data = serializers.serialize("json", Contacto.objects.filter(compania_id=empresa_id, status=True).order_by('nombre'))
 	return HttpResponse(data, content_type='application/json')
+
+@csrf_exempt
+def add_investigacion(request):
+	print(request.method)
+	print(request.body)
+	return JsonResponse({'status':'success'})
+	
