3b0831379d2d6dabc1452090d1aa66a3d5d791bb
jaime <garciadiazjaime@gmail.com>
2021-07-27 21:48:57 -0500

    feat: [https://trello.com/c/R40SnzVb] add investigacion

--- project/app/api/views.py
+++ project/app/api/views.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import json
+
 from django.shortcuts import HttpResponse
-from app.compania.models import Contacto
 from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.contrib.auth.models import User
+
+from app.investigacion.models import Investigacion
+from app.compania.models import Contacto
+from app.entrevista.controllerpersona import ControllerPersona
 
 ''' ----------------- EMPRESA -----------------'''
 
@@ -14,7 +20,19 @@ def empresa_get_contactos(request, empresa_id):
 
 @csrf_exempt
 def add_investigacion(request):
-	print(request.method)
-	print(request.body)
+	if request.method != 'POST':
+		return JsonResponse({'status':'empty'})
+
+	investigacion_id = 304
+	user_id = 2
+
+	investigacion = Investigacion.objects.get(id=investigacion_id)
+	user = User.objects.get(id=user_id)
+	data = {
+		'candidato': json.loads(request.body.decode('utf8').replace("'", '"'))
+	}
+	candidato = ControllerPersona()
+	candidato.saveAllData(investigacion, data, None, user)
+
 	return JsonResponse({'status':'success'})
 	
