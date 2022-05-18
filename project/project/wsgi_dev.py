e4f6e86e159946125e371b030f5260afa32385f4
J. Hernán Ramírez R <hr@hernanramirez.info>
2022-05-17 21:08:14 -0400

    Ajuste

--- project/project/wsgi_dev.py
+++ project/project/wsgi_dev.py
@@ -16,7 +16,7 @@ framework.
 import os
 #import newrelic.agent
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.dev")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
