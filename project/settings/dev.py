79cce6bd1a4738f85957407fb5b5c33a197136e2
J. Hernán Ramírez R <hr@hernanramirez.info>
2022-05-17 21:15:14 -0400

    Setting

--- project/settings/dev.py
+++ project/settings/dev.py
@@ -10,7 +10,7 @@ env = environ.Env()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.environ['CONTAKTO_DB_NAME'],
+        'NAME': os.environ['CONTAKTO_DB_NAME_DEV'],
         'USER': os.environ['CONTAKTO_DB_USER'],                      # Not used with sqlite3.
         'PASSWORD': os.environ['CONTAKTO_DB_PASSWORD'],                  # Not used with sqlite3.
         'HOST': os.environ['MYSQL_CONTAKTO_PORT_3306_TCP_ADDR']                      # Set to empty string for localhost. Not used with sqlite3.
