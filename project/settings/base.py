befcbfd818babf98c540f59b810dcbd74d93495f
J. Hernán Ramírez R <hr@hernanramirez.info>
2022-05-26 21:09:24 -0400

    Ajuste permisología

--- project/settings/base.py
+++ project/settings/base.py
@@ -206,7 +206,6 @@ OAUTH2_PROVIDER = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
-        # 'oauth2_provider.ext.rest_framework.OAuth2Authentication', 
         # 'rest_framework.authentication.TokenAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
        
