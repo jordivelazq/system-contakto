1524ea62db65da7a2ae6481ae54e367e4051b7cf
Jordi Velazquez <jordi.velazquez@outlook.com>
2023-07-08 12:48:45 -0600

    Se quita el filtro de paginación de Datatables

--- project/settings/base.py
+++ project/settings/base.py
@@ -235,17 +235,6 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "DEFAULT_RENDERER_CLASSES": (
-        "rest_framework.renderers.JSONRenderer",
-        "rest_framework.renderers.BrowsableAPIRenderer",
-        "rest_framework_datatables.renderers.DatatablesRenderer",
-    ),
-    "DEFAULT_FILTER_BACKENDS": (
-        "rest_framework_datatables.filters.DatatablesFilterBackend",
-    ),
-    "DEFAULT_PAGINATION_CLASS":
-    "rest_framework_datatables.pagination.DatatablesPageNumberPagination",
-    "PAGE_SIZE": 50,
 }
 
 
