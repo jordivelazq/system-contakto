f128ccb0d5fde40b1172c7fd1e12edf30e7d9837
Jordi Velazquez <jordi.velazquez@outlook.com>
2023-07-08 12:50:48 -0600

    Cambio en base.py

--- project/settings/base.py
+++ project/settings/base.py
@@ -235,6 +235,10 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ),
 }
 
 
