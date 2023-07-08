dd8eb76a08526212e0e7c359c0ccd42aa2d47267
Jordi Velazquez <jordi.velazquez@outlook.com>
2023-07-08 12:53:08 -0600

    cambio en base.py

--- project/settings/base.py
+++ project/settings/base.py
@@ -238,7 +238,13 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": (
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
+        "rest_framework_datatables.renderers.DatatablesRenderer",
     ),
+    "DEFAULT_FILTER_BACKENDS": (
+        "rest_framework_datatables.filters.DatatablesFilterBackend",
+    ),
+    "DEFAULT_PAGINATION_CLASS":
+    "rest_framework_datatables.pagination.DatatablesPageNumberPagination",
 }
 
 
